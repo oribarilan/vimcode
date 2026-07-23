a273e4dcc8859a5702426e036d103cb7a824cfdf
ori <oribarilan@gmail.com>
2026-07-23 18:13:55 +0300

    refactor: reduce vim barrel to public surface, drop dead _CONSUME

--- src/vim/index.ts
+++ src/vim/index.ts
@@ -1,5 +1,3 @@
-import type { HandlerResult } from "./types";
-
 export { handleInsertKey } from "./insert";
 export { handleNormalKey } from "./normal";
 export { createVimState, finishOneShotIfComplete, toggleVimMode } from "./state";
@@ -7,5 +5,3 @@ export { endOfWord } from "./text";
 export type { Action, KeyEvent, PromptAccess, VimState } from "./types";
 export { translateKey } from "./util";
 export { handleVisualKey } from "./visual";
-
-const _CONSUME: HandlerResult = { consume: true, actions: [] };
